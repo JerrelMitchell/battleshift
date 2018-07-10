84b01203d1b264cb05f9e443bca369865d442ee4
JerrelMitchell <jerrelmitchell@ymail.com>
2018-07-09 20:35:42 -0600

    add tests for email and email links

--- spec/features/user_can_activate_account_spec.rb
+++ spec/features/user_can_activate_account_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Account Activation' do
   it "sends an email to a newly registered account where user can activate their account" do
-    last_email = ActionMailer::Base.deliveries.last
     activation_link = 'Visit here to activate your account.'
     thanks_message = 'Thank you! Your account is now activated.'
     user = { name: 'Sally Test', email: 'sally@example.com', password: 'sallyspassword' }
+    email_link = /Welcome to Battleshift. Activate your account here!/
 
     visit '/register'
 
@@ -15,12 +15,11 @@ RSpec.describe 'Account Activation' do
     fill_in 'Password confirmation', with: user[:password]
     click_button 'Submit'
 
-    expect(last_email.body.raw_source).to have_link(activation_link)
-    click_link activation_link
-    expect(page).to have_current_path('/confirmation')
-    expect(page).to have_content(thanks_message)
+    email = ActionMailer::Base.deliveries.last
 
-    visit '/dashboard'
-    expect(page).to have_content('Status: Active')
+    expect(email.from).to eq(['no-reply@example.com'])
+    expect(email.to).to eq([user[:email]])
+    expect(email.subject).to eq('Welcome to Battleshift. Activate your account here!')
+    expect(email.html_part.body).to match(email_link)
   end
 end
--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe ActivationNotifierMailer do
   it 'should contain correct content when sent' do
     recipient = User.create(email: 'example@example.com', name: 'Example', password: 'supersecret')
-    email = ActivationNotifierMailer.inform(recipient.email)
+    email = ActivationNotifierMailer.inform(recipient)
+    email_link = /Welcome to Battleshift. Activate your account here!/
 
-    expect(email.from).to eq('no-reply@example.com')
-    expect(email.to).to eq(recipient.email)
+    expect(email.from).to eq(['no-reply@example.com'])
+    expect(email.to).to eq([recipient.email])
     expect(email.subject).to eq('Welcome to Battleshift. Activate your account here!')
-    expect(email.body.to_s).to eq(File.read('/fixtures/example_email'))
+    expect(email.html_part.body).to match(email_link)
   end
 end
