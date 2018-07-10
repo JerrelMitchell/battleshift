baba921fcc375550fa3888ca878a3916e15ab664
JerrelMitchell <jerrelmitchell@ymail.com>
2018-07-10 11:32:58 -0600

    update user to remove commented code, fix spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :password_digest
-  # validates_presence_of :password_confirmation, if: :password_changed?
   validates_uniqueness_of :email
   validates_confirmation_of :password
 
   has_secure_password
+
   enum status: [:inactive, :active]
 
   def activated?
