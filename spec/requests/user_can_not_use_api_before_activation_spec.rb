a34a8d9f989d3e86eab24ad76ee6b3b4da59c0cb
Justine <jjtroyke@gmail.com>
2018-07-10 18:21:11 -0600

    Updated

--- spec/requests/user_can_not_use_api_before_activation_spec.rb
+++ spec/requests/user_can_not_use_api_before_activation_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User Registration' do
+RSpec.describe 'Visiting new game api' do
   it 'they should not be able to use API key prior to account activation' do
     user = User.create!(name: "Mulder", email: "fox@x.com", password: "123", status: 0)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
