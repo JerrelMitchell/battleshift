98b9aebef4d617276d8baedf2c2a6c47b4bfd284
Justine <jjtroyke@gmail.com>
2018-07-10 18:14:54 -0600

    Passed test for user not able to visit new game

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :games, only: [:show] do
+      resources :games, only: [:show, :new, :create] do
         post "/shots", to: "games/shots#create"
       end
     end
--- spec/requests/user_can_not_use_api_before_activation_spec.rb
+++ spec/requests/user_can_not_use_api_before_activation_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe 'User Registration' do
     user = User.create!(name: "Mulder", email: "fox@x.com", password: "123", status: 0)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit "api/v1/games/new"
+    post "/api/v1/games"
 
-    expect(current_page).to have_content("Account has not yet been activated.")
+    returned = JSON.parse(response.body)
+    expect(returned["error"]).to eq("Account has not yet been activated.")
   end
 end
