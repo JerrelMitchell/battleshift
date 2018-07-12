fecec499eeaacbaf526472b3b6f723fb5467990d
JerrelMitchell <jerrelmitchell@ymail.com>
2018-07-12 12:05:11 -0600

    refactor test and ships controller for placing a ship, refactor game start test and game controller for starting a game. all tests passing

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -7,18 +7,23 @@ class Api::V1::Games::ShipsController < ApplicationController
     player = User.find_by(auth_token: request.headers['HTTP_X_API_KEY'])
     payload = JSON.parse(params[:body])
 
-    
+
     # Create a new Ship object with length from request body param
     ship = Ship.new(payload['ship_size'])
 
-    # Create ShipPlacer object with game.player_#_board, ship, 
+    # Create ShipPlacer object with game.player_#_board, ship,
     #   start_space (from req body param), end_space (from req body param)
 
+    if UserGame.find_by(user_id: player.id).player == 'player_1'
+      board = game.player_1_board
+    else
+      board = game.player_2_board
+    end
     # !!!!!!
     # Don't hard code player 1's board, game needs player 1 and player 2 fields
     # !!!!!!
-    placer = ShipPlacer.new(board: game.player_1_board, 
-                            ship: ship, 
+    placer = ShipPlacer.new(board: board,
+                            ship: ship,
                             start_space: payload['start_space'],
                             end_space: payload['end_space'])
 
--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -11,13 +11,19 @@ module Api
       end
 
       def create
-        Game.create!(
+        player_1 = User.find_by(auth_token: request.headers["HTTP_X_API_KEY"])
+        player_2 = User.find_by(email: params[:opponent_email])
+
+        game = Game.create!(
           player_1_board: Board.new(6),
           player_2_board: Board.new(6),
           player_1_turns: 0,
           player_2_turns: 0,
           current_turn: 'challenger'
         )
+
+        game.user_games.create!(user_id: player_1.id, player: 0)
+        game.user_games.create!(user_id: player_2.id, player: 1)
       end
     end
   end
