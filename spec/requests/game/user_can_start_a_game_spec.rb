b480eb6f12cfc5a79db1d4b0a68aa41611cb6fdf
Justine <jjtroyke@gmail.com>
2018-07-11 18:15:38 -0600

    Created test for new game

---
+++ spec/requests/game/user_can_start_a_game_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'GET /api/v1/games' do
+  context 'user starts game' do
+    it 'returns a game with boards' do
+      email = 'catherinezetajones@threatlevel.com'
+      player_1 = User.create!(name: 'Michael Scarn', email: 'goldenfacehunter@scarn.com', password: 'dwigt')
+      User.create!(name: 'Golden Face', email: email, password: 'midnight')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(player_1)
+
+      post "/api/v1/games?api_key=#{player_1.auth_token}&opponent_email=#{email}"
+
+      expect(response).to be_successful
+    end
+  end
+end
