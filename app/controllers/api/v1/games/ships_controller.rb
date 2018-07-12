9e82834f7d6376ab8a7ea8d6842bf52e2ae20cf4
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-11 23:06:55 -0600

    Implementing endpoint for creating a ship is WIP, player board is currently hard coded and needs to be determined by matching found player (using API key) to player 1/2 column

---
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::Games::ShipsController < ApplicationController
+  def create
+    # Find the game using the game_id in params
+    game = Game.find(params[:game_id])
+
+    # Find player using auth_token
+    player = User.find_by(auth_token: request.headers['HTTP_X_API_KEY'])
+    payload = JSON.parse(params[:body])
+
+    
+    # Create a new Ship object with length from request body param
+    ship = Ship.new(payload['ship_size'])
+
+    # Create ShipPlacer object with game.player_#_board, ship, 
+    #   start_space (from req body param), end_space (from req body param)
+    
+    # !!!!!!
+    # Don't hard code player 1's board, game needs player 1 and player 2 fields
+    # !!!!!!
+    placer = ShipPlacer.new(game.player_1_board, ship, payload['start_space'], payload['end_space'])
+
+    # Run ShipPlacer
+    placer.run
+    require 'pry';binding.pry
+  end
+end
