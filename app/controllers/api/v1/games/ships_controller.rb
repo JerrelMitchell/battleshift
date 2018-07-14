8e1b1c05565d51bb8a412659b824c441393f9479
Justine <jjtroyke@gmail.com>
2018-07-14 11:40:08 -0600

    Working through ships remaining count

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -13,7 +13,6 @@ class Api::V1::Games::ShipsController < Api::V1::ApplicationController
     placer.run
 
     game.update(player_1_board: game.player_1_board, player_2_board: game.player_2_board)
-binding.pry
     render json:game, message: "Successfully placed ship with a size of #{payload['ship_size']}. You have #{} ship(s) to place with a size of 2."
   end
 end
--- spec/models/user_game_spec.rb
+++ spec/models/user_game_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe UserGame do
 
   describe 'Validations' do
     it { should validate_presence_of(:player) }
+    it { should validate_presence_of(:ships) }
   end
 end
