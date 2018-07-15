e9fcf10764fb46c107bd88879707be07b8d408ab
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-15 11:16:34 -0600

    Tests new_ship instance method for game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  let(:player_type) { "challenger" }
+  let(:game) {
+    Game.create!( player_1_board: Board.new(4),
+                  player_2_board: Board.new(4),
+                  player_1_turns: 0,
+                  player_2_turns: 0,
+                  current_turn: player_type)
+  }
+
   it "should have player boards" do
-    player_type = "challenger"
-    player_1_board = Board.new(4)
-    player_2_board = Board.new(4)
-    game_attributes = {
-                    player_1_board: player_1_board,
-                    player_2_board: player_2_board,
-                    player_1_turns: 0,
-                    player_2_turns: 0,
-                    current_turn: player_type
-                  }
-
-    game = Game.create!(game_attributes)
-
     expect(game.player_1_board.length).to eq(4)
     expect(game.player_2_board.length).to eq(4)
     expect(game.player_1_turns).to eq(0)
@@ -29,5 +25,27 @@ RSpec.describe Game, type: :model do
   describe 'Relationships' do
     it { should have_many(:user_games) }
     it { should have_many(:users).through(:user_games) }
+  end
+
+  context 'Instance Methods' do
+    describe '#new_ship' do
+      it 'should instantiate a new ship object' do
+        # Create a destroyer
+        ship_length = 3
+        ship = game.new_ship(ship_length)
+
+        expect(ship).to be_a(Ship)
+        expect(ship.length).to eq(3)
+        expect(ship.type).to eq('destroyer')
+
+        # Create a cruiser
+        ship_length = 2
+        ship = game.new_ship(ship_length)
+
+        expect(ship).to be_a(Ship)
+        expect(ship.length).to eq(2)
+        expect(ship.type).to eq('cruiser')
+      end
+    end
   end
 end
